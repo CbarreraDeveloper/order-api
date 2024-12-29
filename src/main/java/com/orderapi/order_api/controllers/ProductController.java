f741f62d90dd1e2491e3caa0dab86374c9a1fe9b
CbarreraDeveloper <barrerac518@gmail.com>
2024-12-29 20:39:47 -0300

    Creando DTO de productos

--- src/main/java/com/orderapi/order_api/controllers/ProductController.java
+++ src/main/java/com/orderapi/order_api/controllers/ProductController.java
@@ -1,5 +1,6 @@
 package com.orderapi.order_api.controllers;
 
+import com.orderapi.order_api.dtos.ProductDTO;
 import com.orderapi.order_api.entity.Product;
 import com.orderapi.order_api.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class ProductController {
@@ -16,9 +18,15 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping(value = "/products/{productId}")
-    public ResponseEntity<Product> findById(@PathVariable("productId")  Long productId){
+    public ResponseEntity<ProductDTO> findById(@PathVariable("productId")  Long productId){
         Product product = productService.findById(productId);
-        return new ResponseEntity<Product>(product, HttpStatus.OK);
+
+        ProductDTO productDTO = ProductDTO.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .price(product.getPrice())
+                .build();
+        return new ResponseEntity<ProductDTO>(productDTO, HttpStatus.OK);
     }
 
     @DeleteMapping(value = "products/{productId}")
@@ -28,21 +36,43 @@ public class ProductController {
     }
 
     @GetMapping(value = "/products")
-    public ResponseEntity<List<Product>> findAll(){
+    public ResponseEntity<List<ProductDTO>> findAll(){
         List<Product> products = productService.findAll();
-        return new ResponseEntity<List<Product>>(products, HttpStatus.OK);
+
+        List<ProductDTO> dtoProducts =  products.stream().map(product -> {
+            return ProductDTO.builder()
+                    .id(product.getId())
+                    .name(product.getName())
+                    .price(product.getPrice())
+                    .build();
+        }).collect(Collectors.toList());
+
+        return new ResponseEntity<List<ProductDTO>>(dtoProducts, HttpStatus.OK);
     }
 
    @PostMapping(value = "/products")
-   public ResponseEntity<Product> create(@RequestBody Product product){
+   public ResponseEntity<ProductDTO> create(@RequestBody Product product){
         Product newProduct = productService.save(product);
-        return new ResponseEntity<Product>(newProduct, HttpStatus.CREATED);
+
+        ProductDTO productDTO = ProductDTO.builder()
+                .id(newProduct.getId())
+                .name(newProduct.getName())
+                .price(newProduct.getPrice())
+                .build();
+        return new ResponseEntity<ProductDTO>(productDTO, HttpStatus.CREATED);
    }
 
     @PutMapping(value = "/products")
-    public ResponseEntity<Product> update(@RequestBody Product product){
+    public ResponseEntity<ProductDTO> update(@RequestBody Product product){
         Product updateProduct = productService.save(product);
-        return new ResponseEntity<Product>(updateProduct, HttpStatus.OK);
+
+        ProductDTO productDTO = ProductDTO.builder()
+                .id(updateProduct.getId())
+                .name(updateProduct.getName())
+                .price(updateProduct.getPrice())
+                .build();
+
+        return new ResponseEntity<ProductDTO>(productDTO, HttpStatus.OK);
     }
 
 }
