7b9cad2ed91eec3226c729023180ed9a526257c7
CbarreraDeveloper <barrerac518@gmail.com>
2024-12-29 17:50:06 -0300

    Creando entidad productos

--- src/main/java/com/orderapi/order_api/entity/Product.java
+++ src/main/java/com/orderapi/order_api/entity/Product.java
@@ -1,13 +1,27 @@
 package com.orderapi.order_api.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+
+@AllArgsConstructor
 @Builder
 @Getter
 @Setter
+@Entity
+@Table(name="PRODUCTS")
 public class Product {
 
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "NAME", nullable = false, length = 100)
     private String name;
+
+    @Column(name = "PRICE", nullable = false)
+    private Double price;
 }
