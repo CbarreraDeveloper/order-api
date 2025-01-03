bab620faa27fb4c83847270129e4cbec09066b93
CbarreraDeveloper <barrerac518@gmail.com>
2025-01-03 10:53:38 -0300

    Crear token JWT

--- src/main/java/com/orderapi/order_api/services/UserService.java
+++ src/main/java/com/orderapi/order_api/services/UserService.java
@@ -9,13 +9,20 @@ import com.orderapi.order_api.exceptions.NoDataFoundException;
 import com.orderapi.order_api.exceptions.ValidateServiceException;
 import com.orderapi.order_api.repository.UserRepository;
 import com.orderapi.order_api.validators.UserValidator;
+import io.jsonwebtoken.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 @Slf4j
 @Service
 public class UserService {
+
+    @Value("${jwt.password}")
+    private String jwtSecret;
 
     @Autowired
     private UserConverter userConverter;
@@ -49,7 +56,9 @@ public class UserService {
                     .orElseThrow(() -> new ValidateServiceException("Usuario o password incorrectos"));
 
             if(! user.getPassword().equals(request.getPassword())) throw new ValidateServiceException("Usuario o password incorrectos");
-            return new LoginResponseDTO(userConverter.fromEntity(user), "TOKEN");
+
+            String token = createToken(user);
+            return new LoginResponseDTO(userConverter.fromEntity(user), token);
         } catch (ValidateServiceException | NoDataFoundException e) {
             log.info(e.getMessage(), e);
             throw e;
@@ -57,6 +66,34 @@ public class UserService {
             log.error(e.getMessage(), e);
             throw new GeneralServiceException(e.getMessage(), e);
         }
+    }
+
+    public String createToken(User user) {
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + (1000*60*60));
+
+        return Jwts.builder()
+                .setSubject(user.getUsername())
+                .setIssuedAt(now)
+                .setExpiration(expiryDate)
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .compact();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token);
+            return true;
+        }catch (UnsupportedJwtException e) {
+            log.error("JWT in a particular format/configuration that does not match the format expected");
+        }catch (MalformedJwtException e) {
+            log.error(" JWT was not correctly constructed and should be rejected");
+        }catch (SignatureException e) {
+            log.error("Signature or verifying an existing signature of a JWT failed");
+        }catch (ExpiredJwtException e) {
+            log.error("JWT was accepted after it expired and must be rejected");
+        }
+        return false;
     }
 
 }
